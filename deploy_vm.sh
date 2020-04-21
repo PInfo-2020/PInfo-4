b420fc8d282e584952f8def613a98aa6cdb02395
Luke.Smith <luke.smith@bluewin.ch>
2020-04-21 14:38:55 +0200

    Corrected misformed helm release name

--- deploy_vm.sh
+++ deploy_vm.sh
@@ -3,14 +3,15 @@
 # This script is ran on the Virtual Machine at: smithlu7@129.194.69.131
 # It deploys the application 'Kernel' using 'helm install' or 'helm upgrade'
 
+RELEASE=fridge-hub
 helm repo add hung-repo https://pinfo-2020.github.io/PInfo-4/
 helm repo update
 
-if helm list --all-namespaces -a | grep -q "fridge_hub"
+if helm list --all-namespaces -a | grep -q $RELEASE
 then
-  echo "fridge_hub exists"
-  helm upgrade fridge_hub hung-repo/microservices
+  echo "$RELEASE exists"
+  helm upgrade $RELEASE hung-repo/microservices
 else
-  echo "fridge_hub doesn't exist"
-  helm install fridge_hub hung-repo/microservices
+  echo "$RELEASE doesn't exist"
+  helm install $RELEASE hung-repo/microservices
 fi
